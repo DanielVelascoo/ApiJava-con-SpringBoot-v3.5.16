227b3f72746e7a7048002d1b813353559b96a7d4
Daniel Velasco <danielcamiloeskratos@gmail.com>
2026-07-15 14:17:38 -0500

    Importaciones no necesarias eliminadas

--- src/main/java/com/api/DanielVelasco/services/ProductoService.java
+++ src/main/java/com/api/DanielVelasco/services/ProductoService.java
@@ -8,7 +8,6 @@ import com.api.DanielVelasco.dto.ProductoRequestDTO;
 import com.api.DanielVelasco.dto.ProductoResponseDTO;
 import com.api.DanielVelasco.entities.Categoria;
 import com.api.DanielVelasco.entities.Producto;
-import com.api.DanielVelasco.exceptions.ProductoNoEncontradoException;
 import com.api.DanielVelasco.exceptions.ResourceNotFoundException;
 import com.api.DanielVelasco.repositories.CategoriaRepository;
 import com.api.DanielVelasco.repositories.ProductoRepository;
